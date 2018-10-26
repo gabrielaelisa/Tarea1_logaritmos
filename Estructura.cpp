6f7204cb5762c4a3df7b89cabf196f099dbdafe6
Pedro Belmonte <pedro.belmonte@ing.uchile.cl>
2018-10-26 14:12:19 -0300

    creacion de map en ordenar

--- Estructura.cpp
+++ Estructura.cpp
@@ -8,17 +8,23 @@ void EstructuraArchivo::add_nodo(Nodo *nodo) {
     string new_line = nodo->como_linea();
     outfile.open(filename);
     outfile << new_line << endl;
-    f.close();
+    outfile.close();
 }
 
 void EstructuraArchivo::ordenar(string atributo, long long M) {
     if (!open) {
-        f.open(file);
+        infile.open(filename);
     }
     long long i = 0;
     string line;
+    map<Value, Nodo> nodos;
     while (i < M) {
-        getline(f, line);
+        getline(infile, line);
+        // deserializar la linea para obtener un nodo
+        string column;
+        Nodo *n;
+        n = new Nodo(&column, new Value(0), 10);
+        nodos[n->mymap()[atributo]] = *n;
         i++;
     }
 }
